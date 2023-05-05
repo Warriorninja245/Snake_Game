8942a3559abc515733299fff03f28e2aae5a4c8d
ElliotGobbert <126508876+ElliotGobbert@users.noreply.github.com>
2023-05-05 07:30:22 -0400

    Create snakePiece.java

---
+++ SnakeGame1/src/snakePiece.java
@@ -0,0 +1,16 @@
+public class snakePiece {
+  private int xCoordinate;
+  private int yCoordinate;
+  
+  public snakePiece(int x, int y) {
+    xCoordinate = x;
+    yCoordinate = y;
+  }
+  public int getX() {
+    return xCoordinate;
+  }
+  public int getY() {
+    return yCoordinate;
+  }
+  
+}
